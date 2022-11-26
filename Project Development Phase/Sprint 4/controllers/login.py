45afff61ae310c17b1ff4d39bac2a6388e86b83b
VASAGAN18 <vasagan182002@gmail.com>
2022-11-26 22:47:20 +0530

    Final Commit with Course Completion

--- Project Development Phase/Sprint 4/controllers/login.py
+++ Project Development Phase/Sprint 4/controllers/login.py
@@ -1,6 +1,7 @@
 from connection import conn
 import ibm_db
 from flask import session
+from sendgrid import Mail, SendGridAPIClient
 
 def loginUser(data):
     email = data['email']
@@ -16,5 +17,22 @@ def loginUser(data):
     if tuple != False:
         response = tuple[0]
         session['userId'] = response
+        message = Mail(
+            from_email='19bcs047@mcet.in',
+            to_emails='karthikseshadri7@gmail.com',
+            subject='A Test from SendGrid!',
+            html_content='New Jobs based on your skillset.'
+        )
+        try:
+            sg = SendGridAPIClient('SENDGRID_API_KEY')
+            response = sg.send(message)
+            code, body, headers = response.status_code, response.body, response.headers
+            print(f"Response Code: {code} ")
+            print(f"Response Body: {body} ")
+            print(f"Response Headers: {headers} ")
+            print("Message Sent!")
+        except Exception as e:
+            print("Error: {0}".format(e))
+                # print(str(response.status_code))
 
     return response    
